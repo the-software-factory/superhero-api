7fb6cbe989300c02e68ad35eb125bd5060e7c841
Fabio Biocchetti <MacBook@MacBook-Pro-di-Fabio.local>
2017-03-15 14:17:39 +0100

    Added login and security

--- src/AppBundle/Controller/SuperheroController.php
+++ src/AppBundle/Controller/SuperheroController.php
@@ -6,6 +6,7 @@ use AppBundle\Form\SuperheroForm;
 use AppBundle\Model\Superhero;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,6 +17,7 @@ class SuperheroController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Security("has_role('ROLE_USER')")
      */
     public function indexAction(Request $request)
     {
@@ -33,12 +35,16 @@ class SuperheroController extends Controller
 
     /**
      * @Route("/detail/{id}", name="detail")
+     * @Security("has_role('ROLE_USER')")
      */
     public function detailAction($id)
     {
         $repository = $this->getDoctrine()->getRepository(Superhero::class);
 
         $superhero = $repository->find($id);
+        if ($superhero === null) {
+            throw $this->createNotFoundException();
+        }
 
         return $this->render(
             'default/detail.html.twig',
@@ -51,6 +57,7 @@ class SuperheroController extends Controller
     /**
      * @Route("/create", name="create")
      * @Method({"GET", "POST"})
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function createAction(Request $request)
     {
@@ -64,23 +71,73 @@ class SuperheroController extends Controller
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($superhero);
             $entityManager->flush();
-            return $this->redirectToRoute('detail', [ 'id' => $superhero->getId() ]);
+            return $this->redirectToRoute('detail', ['id' => $superhero->getId()]);
         }
 
         return $this->render(
             'default/create.html.twig',
             [
-                'form' => $form->createView()
+                'form' => $form->createView(),
             ]
         );
     }
 
     /**
+     * @Route("/edit/{id}", name="edit")
+     * @Method({"GET", "POST"})
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function editAction(Request $request, $id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Superhero::class);
+
+        $superhero = $repository->find($id);
+        if ($superhero === null) {
+            throw $this->createNotFoundException();
+        }
+
+        $form = $this->createForm(SuperheroForm::class, $superhero);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('detail', ['id' => $superhero->getId()]);
+        }
+
+        return $this->render(
+            'default/edit.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function deleteAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Superhero::class);
+
+        $superhero = $repository->find($id);
+        if ($superhero === null) {
+            throw $this->createNotFoundException();
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($superhero);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('homepage');
+    }
+
+    /**
      * @Route("/allHero", name="allHero")
+     * @Security("has_role('ROLE_USER')")
      */
     public function allHeroAction(Request $request)
     {
-        $repository=$this->getDoctrine()->getRepository(Superhero::class);
+        $repository = $this->getDoctrine()->getRepository(Superhero::class);
         $superheroes = $repository->findAll();
         // replace this example code with whatever you need
         return $this->render('default/allHero.html.twig',
